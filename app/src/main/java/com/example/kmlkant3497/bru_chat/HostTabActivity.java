55f940e7184e7b69399194b52fbeaf189a7a08b1
kmlkant9 <kmltechnomagi@gmail.com>
2017-10-24 19:34:38 +0530

    host ip added

--- app/src/main/java/com/example/kmlkant3497/bru_chat/HostTabActivity.java
+++ app/src/main/java/com/example/kmlkant3497/bru_chat/HostTabActivity.java
@@ -1,16 +1,45 @@
 package com.example.kmlkant3497.bru_chat;
 
+import android.net.wifi.WifiManager;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Enumeration;
+import android.text.format.Formatter;
+
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 public class HostTabActivity extends AppCompatActivity {
+
+    private TextView serverStatus;
+
+    // DEFAULT IP
+    public static String SERVERIP = "10.0.2.15";
+
+    // DESIGNATE A PORT
+    public static final int SERVERPORT = 8080;
+
+    WifiManager wmanager;
+    private Handler handler = new Handler();
+
+    private ServerSocket serverSocket;
 
     Button button_disconnect;
     @Override
@@ -22,6 +51,15 @@ public class HostTabActivity extends AppCompatActivity {
         ActionBar ab = getSupportActionBar();
         // Enable the Up button
         ab.setDisplayHomeAsUpEnabled(true);
+
+        //serverip
+        wmanager=(WifiManager) getSystemService(WIFI_SERVICE);
+        SERVERIP = Formatter.formatIpAddress(wmanager.getConnectionInfo().getIpAddress());
+        serverStatus = (TextView) findViewById(R.id.textView_ip);
+
+
+        Thread fst = new Thread(new ServerThread());
+        fst.start();
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
         tabLayout.addTab(tabLayout.newTab().setText("Connections"));
@@ -58,6 +96,50 @@ public class HostTabActivity extends AppCompatActivity {
             }
         });
     }
+
+    public class ServerThread implements Runnable {
+
+        public void run() {
+            try {
+                if (SERVERIP != null) {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            serverStatus.setText("Host IP: " + SERVERIP);
+                        }
+                    });
+
+                } else {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            serverStatus.setText("Couldn't detect internet connection.");
+                        }
+                    });
+                }
+            } catch (Exception e) {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        serverStatus.setText("Error");
+                    }
+                });
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        try {
+            // MAKE SURE YOU CLOSE THE SOCKET UPON EXITING
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -75,4 +157,6 @@ public class HostTabActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
     */
+
+
 }
