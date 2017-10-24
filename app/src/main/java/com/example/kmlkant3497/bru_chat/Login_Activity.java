111634b44eb30a59df7d3d823e50832d62aa164b
Gautam Choudhary <gautamchaudhary2@gmail.com>
2017-10-25 01:05:59 +0530

    Added functionality to JOIN button in Login_Activity DONE: https://github.com/kmlkant9/Bru_Chat/projects/1#card-5304153

--- app/src/main/java/com/example/kmlkant3497/bru_chat/Login_Activity.java
+++ app/src/main/java/com/example/kmlkant3497/bru_chat/Login_Activity.java
@@ -5,14 +5,23 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import static com.example.kmlkant3497.bru_chat.R.id.button_join;
+import static com.example.kmlkant3497.bru_chat.R.id.useLogo;
 
 public class Login_Activity extends AppCompatActivity {
 
+    // helps in Debuggin (via LOG messages)
+    private static final String TAG = "Login_Activity";
+
     public Button button_join;
+    public EditText editText_ip;
+    String ip_address = new String();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +34,38 @@ public class Login_Activity extends AppCompatActivity {
         // Enable the Up button
         ab.setDisplayHomeAsUpEnabled(true);
 
+        editText_ip = (EditText) findViewById(R.id.editText_ip);
+
         button_join = (Button) findViewById(R.id.button_join);
         button_join.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent myIntent = new Intent(Login_Activity.this,ClientActivity.class);
-                startActivity(myIntent);
+                // Retrieve the IP address written in editText
+                ip_address = editText_ip.getText().toString();
+
+                // Check if the IP address entered is correct or not
+                if (isCorrect(ip_address)){
+                    // Open next corresponding activity
+                    Log.d(TAG, "IP-" + ip_address + " is Correct");
+                    Toast.makeText(getApplicationContext(), "Welcome !..", Toast.LENGTH_SHORT).show();
+                    Intent myIntent = new Intent(Login_Activity.this,ClientActivity.class);
+                    startActivity(myIntent);
+                }
+                else {
+                    Log.d(TAG, "IP-" + ip_address + " is InCorrect");
+                    Toast.makeText(getApplicationContext(), "Incorrect IP !..", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+    }
+
+    private boolean isCorrect(String ip_address) {
+        // write code to check correctness of given IP address
+        // Just a dummy code
+        if (ip_address.equals("dummy")) {
+            return false;
+        }
+
+        return true;
     }
 }
